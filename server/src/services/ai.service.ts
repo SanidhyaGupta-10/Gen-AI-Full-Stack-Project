d042415db55bfb7f88d9cb765dee03dc8683e4aa
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-03-07 16:15:17 +0530

    feat: Initialize server project with Bun and add AI service.

--- server/src/services/ai.service.ts
+++ server/src/services/ai.service.ts
@@ -1,17 +1,20 @@
-import { GoogleGenAI } from "@google/genai";
+import OpenAI from "openai";
 
-const api_key = process.env.GEMINI_API_KEY;
+const apiKey = process.env.GROQ_API_KEY;
 
-const ai = new GoogleGenAI({
-    apiKey: api_key
+const client = new OpenAI({
+  apiKey,
+  baseURL: "https://api.groq.com/openai/v1"
 });
 
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-pro",
-    contents: `Hello, How AI worksss`,
+export default async function main() {
+  const response = await client.chat.completions.create({
+    model: "llama-3.1-8b-instant",
+    messages: [
+      { role: "user", content: "Hello, how does AI work?" }
+    ]
   });
-  console.log(response.text);
+
+  console.log(response.choices[0]?.message.content);
 }
 
-export default main;
