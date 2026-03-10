4e78ad00b77af198a5ba845b1ef04b85b78ebfeb
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-03-10 16:43:58 +0530

    Server resolve

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,14 +3,16 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
+    if (!MONGODB_URI) {
+        throw new Error("MONGODB_URI is not defined");
+    }
+
     try {
-        if(!MONGODB_URI){
-            throw new Error("MONGODB_URI is not defined");
-        }
         await mongoose.connect(MONGODB_URI);
         console.log("MongoDB connected");
     } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
+        console.error("MongoDB connection failed:", error);
+        process.exit(1);
     }
 };
 
